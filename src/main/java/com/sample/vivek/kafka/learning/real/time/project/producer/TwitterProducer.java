bfd172ee195f430e6861612a8199f3cf6f957a2d
Vivek Kumar Gupta <vivek.cse.gupta@gmail.com>
2020-07-28 17:05:38 +0530

    Added Properties related to Safe producer where it will not miss acks

--- src/main/java/com/sample/vivek/kafka/learning/real/time/project/producer/TwitterProducer.java
+++ src/main/java/com/sample/vivek/kafka/learning/real/time/project/producer/TwitterProducer.java
@@ -116,6 +116,12 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        // Properties for Safe Producer
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG,"true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG,"all");
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION,"5");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG,Integer.toString(Integer.MAX_VALUE));
+
         return new KafkaProducer<String, String>(properties);
     }
 
